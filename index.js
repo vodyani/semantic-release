d760cf487b79db5aeeb79c6e7534c61e876dc7be
John Denver <chogathkk@gmail.com>
2022-07-08 11:41:17 +0800

    feat: first commit 🌈

---
+++ index.js
@@ -0,0 +1,31 @@
+modules.exports = {
+  "branches": [
+    {
+      "name": "beta",
+      "prerelease": true
+    },
+    {
+      "name": "alpha",
+      "prerelease": true
+    },
+    "master"
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "package-lock.json",
+          "package.json",
+          "CHANGELOG.md"
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github"
+  ]
+}
